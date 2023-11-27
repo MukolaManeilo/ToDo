d5226776541c23c6ff6deec307d5bfe07a5de4f3
KolyaManeilo <kolamanejlo@gmail.com>
2023-11-27 14:51:30 +0200

    Add HomeController and Layout

--- ToDo/ToDo/Program.cs
+++ ToDo/ToDo/Program.cs
@@ -5,9 +5,13 @@ namespace ToDo
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}");
 
             app.Run();
         }
